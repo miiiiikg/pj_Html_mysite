99a29c6996b31e1b31701c7acd3f7f06b5e1e37e
miiiiikg <58206099+miiiiikg@users.noreply.github.com>
2020-02-04 17:25:55 +0900

    html 로그인 유효성 체크 완성
    -이름, 주소, 이메일, 휴대전화 유효성체크

--- bonita/js/validation.js
+++ bonita/js/validation.js
@@ -41,6 +41,10 @@ var joinValidate = {
 			code : 0 ,
 			desc : '사용가능한 비밀번호입니다.'
 		},
+		equal_success_pw : {
+			code : 10,
+			desc : '사용가능한 비밀번호입니다.'
+		},
 		invalid_pw : {
 			code : 3,
 			desc : '비밀번호는 8자 이상이여야 하며, 숫자/대문자/소문자/특수문자를 모두 포함해야 합니다. '
@@ -57,7 +61,7 @@ var joinValidate = {
 			code : 6,
 			desc : '입력하신 비밀번호가 일치하지 않습니다.'
 		}, 
-		equal_pw : {
+		equal_pw : { // 비밀번호 변경할때
 			code : 7,
 			desc : '현재비밀번호와 다르게 입력해주세요.' 
 		},
@@ -71,9 +75,9 @@ var joinValidate = {
 			code : 3,
 			desc : '이름은 표준한글만 입력가능합니다.'
 		},
-		lengh_name : {
+		length_name : {
 			code : 4, 
-			desc : '이름은 2자이상 ~ 4자이하만 가능합니다.'
+			desc : '이름은 2자이상 ~ 20자이하만 가능합니다.'
 		}, 
 	
 		// phone
@@ -101,8 +105,27 @@ var joinValidate = {
 		invalid_email : {
 			code : 3,
 			desc : '올바른 이메일을 입력해주세요.'
+		},
+		// address
+		success_email : {
+			code : 0,
+			desc : '확인되었습니다. :)'
+
+		},
+		empty_post : {
+			code : 3,
+			desc : '우편번호 찾기를 클릭하고 값을 입력해주세요'
+		},
+		empty_detail : {
+			code : 4,
+			desc : '상세주소를 입력해주세요.'
+
+		},
+		invalid_addr : {
+			code : 5,
+			desc : '올바른 주소를 입력해주세요.'
 		}
-	},
+ 	},
 	// 아이디 유효성 체크
 	checkId : function(id) {
 		var regEmpty = /\s/g; //공백문자 
@@ -127,7 +150,7 @@ var joinValidate = {
 			}
 
 	},
-	checkPw : function(pw) {
+	checkPw : function(pw ,rpw) {
 		var regEmpty = /\s/g; // 공백문자
 		// regpw 값이 들어오면 ture값 
 		var regpw = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
@@ -144,11 +167,97 @@ var joinValidate = {
 			return this.resultCode.stream_pw;
 		} else if (regHangle.test(pw)) { // 5. 한글 사용 체크 
 			return this.resultCode.hangle_pw;
-		} else {
+		} else if (rpw != '' || rpw.length != 0) { // 6. 비밀번호재확인 값이 있으면(1번이랑 반대)
+			if(pw == rpw) {
+				return this.resultCode.equal_success_pw;
+			} else {
+				return this.resultCode.other_pw;
+			}
+		} else {	
 			return this.resultCode.success_pw;
+		}
+
+	},
+	checkRpw : function(pw, rpw, pwFlag) {
+		// 비밀번호의 유효성체크를 통과한 값과ㅣ
+		// 비밀번호 재확인 값이 같다면
+		// 비밀번호 재확인 값은 유효성체크를 할 필요가 없음
+		if (rpw == '' || rpw.length == 0) { // 1. 값이 있는지 체크
+			return this.resultCode.empty_val;
+		} else if (!pwFlag) { // 2. pw == rpw가 같은지 비교
+			return this.resultCode.invalid_pw;
+		} else { // 3. pw == rpw가 같은지 비교
+			if(pw == rpw && pwFlag) {
+				return this.resultCode.equal_success_pw;
+			} else {
+				return this.resultCode.other_pw;
+			}
 			
 		}
-	}
 
+	},
+	checkName : function(name) {
+			var regEmpty = /\s/g; // 공백문자
+			var regName = /^[a-zA-Z가-힣]+$/;
+
+			if(name == '' || name.length == 0) { // 1. 값의 유무
+				return this.resultCode.empty_val;
+			} else if (name.match(regEmpty)) { // 2. 공백 유무
+				return this.resultCode.space_length_val;
+			} else if (name.length < 2 || name.length > 20) {// 
+				return this.resultCode.length_name;
+			} else if (!name.match(regName)) {
+				return this.resultCode.invalid_name;
+			}  else { // 통과
+				return this.resultCode.success_name;
+			}
+
+
+	}, 
+	checkPhone : function(phone) {
+		var regEmpty = /\s/g;
+		var regPhone = /^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$/;
+
+		if (phone == '' || phone.length == 0) {
+			return this.resultCode.empty_val;
+		} else if (phone.match(regEmpty)) {
+			return this.resultCode.space_length_val;
+		} else if (!phone.match(regPhone)) {
+			return this.resultCode.invalid_phone;
+		} else {
+			return this.resultCode.success_phone;
+		}
+	},
+	checkEmail :function(email) {
+		var regEmpty = /\s/g;
+		var regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+		if(email == '' || email.length == 0) { // 1. 값유무
+			return this.resultCode.empty_val;
+		} else if (email.match(regEmpty)) {  // 2. 공백체크
+			return this.resultCode.space_length_val;
+		} else if (!email.match(regEmail)) { // 3. 정규식체크
+			return this.resultCode.invalid_email;
+		} else  {
+			return this.resultCode.success_email;
+		}
+	},
+	checkAddr : function(addrDetail, addrPost) {
+		// 영어대문자, 영어소문자, 한글, -, 공백외에 전부 체크
+		var regAddr = /^[a-zA-Z0-9가-힣-\s]+$/;
+
+		if (addrPost == '' || addrPost.length == 0) {
+			return this.resultCode.empty_post;
+		} else if (addrDetail == '' || addrDetail.length == 0) {
+			return this.resultCode.empty_detail;
+		} else if (!addrDetail.match(regAddr)) { // 3.정규식체크
+			return this.resultCode.invalid_addr;
+		}
+		 else {
+			return this.resultCode.success_email;
+		}
+
+	} 
 
 }
+
